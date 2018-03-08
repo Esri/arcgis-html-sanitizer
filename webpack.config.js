1e69a0f96bc4eebe1ea62cccde2a30f693ca0625
ssylvia <ssylvia@esri.com>
2018-03-08 14:40:53 -0500

    Fix UMD output and add copywrite banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const pkg = require('./package');
+const webpack = require('webpack');
 
 module.exports = (env, argv) => {
   return {
@@ -17,11 +18,24 @@ module.exports = (env, argv) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
     },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner:
+          `${pkg.name} - v${pkg.version} - ${new Date().toString()}` +
+          '\n' +
+          `Copyright (c) ${new Date().getFullYear()} - Environmental Systems Research Institute, Inc.` +
+          '\n' +
+          `${pkg.license}`
+      })
+    ],
     output: {
       filename: `${pkg.name.replace('@esri/', '')}${
         argv.mode === 'production' ? '.min' : ''
       }.js`,
-      path: path.resolve(__dirname, 'dist/umd')
+      path: path.resolve(__dirname, 'dist/umd'),
+      library: 'Sanitizer',
+      libraryExport: 'Sanitizer',
+      libraryTarget: 'umd'
     }
   };
 };
