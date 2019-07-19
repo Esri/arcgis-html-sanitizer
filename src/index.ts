2ddc9b3bef3385be79b2859f9d44188e6bdb1268
Blake Stearman <bstearman@esri.com>
2019-07-19 09:47:13 -0700

    Updates to match supported ArcGIS Online HTML. * Added ol and hr tags. * Updated div tag supported attributes to style and align.

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ export class Sanitizer {
     audio: ["autoplay", "controls", "loop", "muted", "preload", "src"],
     span: ["style"],
     table: ["width", "height", "cellpadding", "cellspacing", "border", "style"],
-    div: ["style", "class"],
+    div: ["style", "align"],
     font: ["size", "color", "style"],
     tr: ["height", "valign", "align", "style"],
     td: [
@@ -82,6 +82,8 @@ export class Sanitizer {
     br: [],
     li: [],
     ul: [],
+    ol: [],
+    hr: [],
     tbody: []
   };
   public readonly arcgisFilterOptions: XSS.IFilterXSSOptions = {
